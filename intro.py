a1929c09d2c137d2ee8a55edbe83c5536addd954
Md Yousuf <129869626+yousuf-codes@users.noreply.github.com>
2024-09-12 19:13:00 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("My Intro")
+
+st.header("About Me")
+st.write("Hello! My name is Md Yousuf Azam , and I'm a CS student.")
+st.write("I'm excited to share my intro with you!")
+
+
+st.header("Bio")
+st.write("I'm a student with a passion for AI developer.")
+st.write("I'm excited to share my skills and experiences with you!")
+
+st.header("Skills")
+st.write("Programming languages: Python, C, C++")
+st.write("Data science tools: NumPy, Pandas, Matplotlib")
+st.write("Machine learning frameworks: TensorFlow")
